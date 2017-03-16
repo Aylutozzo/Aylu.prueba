b01483ff0f40086cd9f897b8c8517c7b92b7bd9f
Aylu <aylu2604gmail.com>
2017-03-16 11:57:40 -0300

    final jueves

---
+++ ej1/alumnos/ej1.py
@@ -0,0 +1,20 @@
+class ALumno (object):
+    nombre = " "
+    apellido = " "
+    fecha_nac= None
+    lista_notas = [ ]
+
+    def setNombre(self,n):
+        self.fnombre =str(n)
+
+    def setApellido(self,a):
+        self.apellido = str(t)
+
+    def setfecha_nac(self, Fecha_n):
+        self.fecha_nac= str(fecha_n)
+
+    def Agregar_nota (self, agreg_nota):
+        self.Agregar_nota= str (agreg_nota)
+
+
+
---
+++ ej1/alumnos/programa.py
@@ -0,0 +1,9 @@
+import datetime
+
+from alumnos.ej1 import ej1:
+
+Alumno = ej1 ()
+Alumno.nombre = ("aylu")
+Alumno.apellido = ("tozzo")
+
+
