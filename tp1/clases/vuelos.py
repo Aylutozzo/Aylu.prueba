4dafcefdfe964bfc0d63d186bba6131c59e7f2f6
Aylu <aylu2604gmail.com>
2017-06-02 10:55:14 -0300

    tp1

--- tp1/clases/vuelos.py
+++ tp1/clases/vuelos.py
@@ -12,11 +12,9 @@ class Vuelos (object):
 
     def __init__(self):
         self.lista_pasajeros = []
-
-    def __init__(self):
         self.lista_tripulantes = []
 
-    def agregarPasajeros(self,):
+    def agregarPasajeros(self):
         self.lista_pasajeros.append()
 
     def agregarTripulantes(self):
@@ -34,4 +32,52 @@ class Vuelos (object):
     def Destino(self,destino):
         self.destino=destino
 
-    v = open("/home/alumno/descargas/vuelos.dat", "r")
+
+    def Nomina (self):
+        lista_personas=[]
+        for item in self.lista_pasajeros:
+            self.lista_personas.append(item)
+        for item in self.lista_tripulantes:
+            self.lista_personas.append(item)
+
+        return lista_personas
+
+    def Personas_vip_especiales(self):
+        lista_Pvip=[]
+        lista_Pespecial=[]
+        for item in self.lista_pasajeros:
+            if item.Vip==True:
+                self.lista_Pvip.append(item)
+            if item.necesidadEspecial!= None
+                self.lista_Pespecial.append(item)
+
+        return lista_Pespecial , lista_Pvip
+
+    def sinTriupulacionMinima(self):
+        Tripulacion_minima=True
+        if len(self.lista_tripulantes)<self.avion.cantidad_tripulantes
+            Tripulacion_minima= False
+
+        return Tripulacion_minima
+
+    def Persona_mas_joven(self):
+        aux = self.lista_pasajeros[1].fecha_nac
+        persona_aux = None
+        for item in self.lista_pasajeros:
+            if item.fecha_nac > aux:
+                aux = item.fecha_nac
+                persona_aux = item
+        return persona_aux
+
+
+
+    def EstaAutorizado(self):
+        autorizado = None
+        for item in self.lista_tripulantes:
+            for item2 in item.modelos_avion:
+                if item2 == self.avion.codunico:
+                    aux = True
+                aux = False
+            return autorizado
+
+
--- tp1/programa.py
+++ tp1/programa.py
@@ -11,39 +11,65 @@ lista_pasajero=[]
 lista_tripulantes=[]
 aux=None
 
-personas=open("/home/alumno/Descargas/personas.dat","r")
-for line in personas:
-    lista_personas = line.split("|")
-    unPasajero= Pasajero()
-    unServicio=Servicio()
+def AgregarAviones():
+    a = open("/home/alumno/Descargas/aviones.dat", "r")
+    for line in a:
+        unAvion=Avion()
+        a=line.split("|")
+        unAvion.Codigo(a[0])
+        unAvion.cantidadPsajero(a[1])
+        unAvion.cantidadTripulantes(a[2])
+    aviones.closes
 
-    if lista_personas[0]== "pasajero":
-        unPasajero.tipo = lista_personas[0]
-        unPasajero.nombre = lista_personas[1]
-        unPasajero.apellido = lista_personas[2]
-        unPasajero.fecha_nacimiento = lista_personas[3]
-        unPasajero.dni = lista_personas[4]
-        unPasajero.vip = lista_personas[5]
-        # unPasajero.necesidad_especial=lista_pasajero[6]
-       lista_pasajero.append(unPasajero)
+def AgregarPersonas():
+    p=open("/home/alumno/Descargas/personas.dat","r")
+    for line in p:
+        unaPiloto=Piloto()
+        unPasajero= Pasajero()
+        unServicio=Servicio()
+        p=line.split("|")
+        if p[0]== "Pasajero":
+            unPasajero.setNombre (p[1])
+            unPasajero.setApellido (p[2])
+            unPasajero.fechaNacimiento (p[3])
+            unPasajero.Dni = (p[4])
+            unPasajero.Vip = (p[5])
+            unPasajero.necesidadEspeciall(p[6])
+            p.append(unPasajero)
 
-    elif lista_personas[0]=="servicio":
-        unServicio.nombre=lista_personas[1]
-        unServicio.apellido=lista_personas[2]
-        unServicio.fecha_nacimiento=lista_personas[3]
-        unServicio.dni=lista_personas[4]
-       # unServicio.codigo=lista_personas[5].split(",")
-        unServicio.idioma=lista_personas[6]
-        lista_tripulantes.append(unServicio)
+        elif p[0]=="servicio":
+            unServicio.setNombre(p[1])
+            unServicio.setApellido(p[2])
+            unServicio.fechaNacimiento(p[3])
+            unServicio.Dni(p [4])
+            x.p[5].split(",")
+            for imten in Codigo
+                if item.Codigo==
+            unServicio.Codigo=(p[5]).split(",")
+            unServicio.Idioma=(p[6])
+            p.append(unServicio)
 
 
-    elif lista_personas[0]=="piloto":
-        unPiloto.nombre=lista_personas[1]
-        unPiloto.apellido=lista_personas[2]
-        unPiloto.fecha_nacimiento=lista_personas[3]
-        unPiloto.dni=lista_personas[4]
-        #unPiloto.codigo=lista_personas[5].split(",")#
-
+        elif p[0]=="piloto":
+            unPiloto.setNombre(p[1])
+            unPiloto.setApellido(p[2])
+            unPiloto.fechaNacimiento(p[3])
+            unPiloto.Dni(p[4])
+            x.p[5].split(",")
+            for imten in lista_aviones:
+                if avion.codigo== item:
+                    unaPiloto.Codigo (avion)
+            p.append(unPiloto)
 
 personas.close()
+
+def AgregarVuelos():
+    v= open("/home/alumno/Descargas/vuelos.dat."r"")
+    unVuelo=Vuelos
+    v=line.split("|")
+    unVuelo.Avion(v[0])
+    unVuelo.Dia(v[1])
+    unVuelo.Hora(v[2])
+    unVuelo.Destino(v[3])
+    unVuelo.Origen(v[4])
 
